c250f75a371d02028fe95fca8b64fcaa4ca8ad53
aquilesValerio <aquilesvalerio@gmail.com>
2022-09-28 20:48:27 -0300

    first commit

---
+++ backend/src/main/java/com/sweetHome/pocexception/PocExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.sweetHome.pocexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocExceptionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sweetHome/pocexception/PocExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetHome.pocexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
